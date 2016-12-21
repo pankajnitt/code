256324a833bdf81288ccf5e716add10aeb801287
pankajkumar <pankajkumar@olacabs.com>
2016-12-22 01:14:05 +0530

    21_12

---
+++ c++/21_12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+		int n, x;
+		cin>>n>>x;
+		vector<int> a(n);
+		for(int i = 0; i< n; i++) {
+			cin>>a[i];
+		}
+		sort(a.begin(), a.end());
+		bool found = false;
+		for(int i=0;i<n;i++){
+			if(binary_search(a.begin() + i , a.end(), x - a[i])){
+				cout<<"Yes\n";
+				found = true;
+				break;
+			}
+		}
+		if(!found){
+		cout<<"No\n";
+		}
+	}
+	return 0;
+}
